40759201a869b0bb1065fe26b00d459dfd36e7ea
JOYSTON-LEWIS <joyston2010@gmail.com>
2025-08-21 21:08:05 +0530

    updating route for second microservice

--- backend/hello-service/index.js
+++ backend/hello-service/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3002;
 app.use(express.json());
 app.use(cors());
 
-app.get("/hello", (req, res) => {
+app.get("/hellos", (req, res) => {
   res.json({ message: "Hello from Hello Service 🚀" });
 });
 
